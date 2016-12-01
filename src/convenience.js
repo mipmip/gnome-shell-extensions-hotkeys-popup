4ecf61a6204cc8fed2fa782f0f464a61f00713a3
Kyle Robertze <krobbertze@gmail.com>
2016-12-01 11:29:42 +0200

    fixes translations

--- src/convenience.js
+++ src/convenience.js
@@ -10,10 +10,8 @@ function initTranslations(domain) {
     let localeDir = extension.dir.get_child('locale');
     if (localeDir.query_exists(null)) {
         Gettext.bindtextdomain(domain, localeDir.get_path());
-        global.log("Local install : " + domain + ", " + localeDir.get_path());
     } else {
         Gettext.bindtextdomain(domain, Config.LOCALEDIR);
-        global.log("Global install");
     }
 }
 
