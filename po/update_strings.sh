e03b46f9c9da2684e3e94a6187c20e353f25eaec
Kyle Robertze <krobbertze@gmail.com>
2016-12-05 11:57:08 +0200

    removed autotools

--- po/update_strings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCRIPTDIR=`dirname $0`
-xgettext  --from-code=UTF-8 -k_ -kN_  -o Shortcuts.pot "$SCRIPTDIR"/../src/*.js "$SCRIPTDIR"/../src/schemas/*.xml
-
-for fn in *.po; do
-	msgmerge -U "$fn" Shortcuts.pot
-done
