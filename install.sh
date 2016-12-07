93884999e2e923480ebdbaa3b00452fe1bb22518
Kyle Robertze <krobbertze@gmail.com>
2016-12-07 11:36:13 +0200

    added zip option to install script

--- install.sh
+++ install.sh
@@ -8,20 +8,74 @@ if [ "$UID" = "0" ]; then
 fi
 
 NAME=Shortcuts\@kyle.aims.ac.za
-DEST=~/.local/share/gnome-shell/extensions/$NAME
 
-echo 'Compiling translations...'
-for po in locale/*/LC_MESSAGES/*.po; do
-    msgfmt -cv -o ${po%.po}.mo $po;
-done
+function compile-translations {
+    echo 'Compiling translations...'
+    for po in locale/*/LC_MESSAGES/*.po; do
+        msgfmt -cv -o ${po%.po}.mo $po;
+    done
+}
 
-echo 'Compiling preferences...'
-glib-compile-schemas --targetdir=src/schemas src/schemas
+function compile-preferences {
+    if [ -d src/schemas ]; then
+        echo 'Compiling preferences...'
+        glib-compile-schemas --targetdir=src/schemas src/schemas
+    else
+        echo 'No preferences to compile... Skipping'
+    fi
+}
 
-echo 'Installing...'
-if [ -z $DEST ]; then
-    mkdir $DEST
-fi
-cp -r src/* locale $DEST/
+function make-local-install {
+    DEST=~/.local/share/gnome-shell/extensions/$NAME
 
-echo 'Done'
+    compile-translations
+    compile-preferences
+
+    echo 'Installing...'
+    if [ ! -d $DEST ]; then
+        mkdir $DEST
+    fi
+    cp -r src/* locale $DEST/
+
+    echo 'Done'
+}
+
+function make-zip {
+    if [ -d build ]; then
+        rm -r build
+    fi
+    mkdir build
+    compile-translations
+    compile-preferences
+    echo 'Coping files...'
+    cp -r LICENSE README.md src/* locale build/
+    echo 'Creating archive..'
+    cd build
+    zip -r ../"$NAME".zip ./*
+    cd ..
+    rm -r build
+    echo 'Done'
+}
+
+function useage() {
+    echo 'Useage: ./install.sh COMMAND'
+    echo 'COMMAND:'
+    echo "  local-install  install the extension in the user's home directory"
+    echo '                 under ~/.local'
+    echo '  zip            Creates a zip file of the extension'
+}
+
+case "$1" in
+    "local-install" )
+        make-local-install
+        ;;
+
+    "zip" )
+        make-zip
+        ;;
+
+    * )
+        useage
+        ;;
+esac
+exit
